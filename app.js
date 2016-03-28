62b5d41578d8421c07d8aa8298c754580524ddcf
chuawh <weihong.chua@voxeolabs.com>
2016-03-29 07:49:39 +0800

    update

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
