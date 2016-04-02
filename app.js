6969ffeb0bc673b4291ab6d7dbd7c2eecf635add
chuawh <weihong.chua@voxeolabs.com>
2016-04-02 15:09:02 +0800

    update

--- app.js
+++ app.js
@@ -47,21 +47,6 @@ app.post('/users', function (req, res,next) {
 });
 
 
-function postHttp(msg){
-
-var token='6248436a7461745a50424e6f615363576162666672796e436871794e68454a514a444a4162666e784b565851';
-
-request({
-    url: 'https://api.tropo.com/1.0/sessions?action=create' + '&token=' + token + '&txt=' + msg,
-    method: 'POST', 
-}, function(error, response, body){
-    if(error) {
-        console.log(error);
-    } else {
-        console.log(response.statusCode, body);
-    }
-});
-};
 
 function getMsg(msgId){
 
@@ -81,19 +66,45 @@ request({
         console.log('The response code is ' + res.statusCode + '\n')
         console.log('The json body is: ' + '\n' + res.body + '\n');
     }
-    var textMsg=JSON.parse(res.body);
-    console.log('The chat message is: ' + textMsg.text + '\n');
+    var jsonText=JSON.parse(res.body);
+    console.log('The chat message is: ' + jsonText.text + '\n');
     //console.log('Ther email is: ' + textMsg.personEmail + '\n');
     //postHttp(textMsg.text);
-    postMsg(textMsg.roomId, textMsg.text);    
+    //postMsg(textMsg.roomId, textMsg.text);    
 
 });
 
 };
 
+
+
+function postHttp(msg){
+
+var token='6248436a7461745a50424e6f615363576162666672796e436871794e68454a514a444a4162666e784b565851';
+
+request({
+    url: 'https://api.tropo.com/1.0/sessions?action=create' + '&token=' + token + '&txt=' + msg,
+    method: 'POST', 
+}, function(error, response, body){
+    if(error) {
+        console.log(error);
+    } else {
+        console.log(response.statusCode, body);
+    }
+});
+};
+
+
+
+
+
 function postMsg(roomId,msgContent){
 
 var token='Bearer MTU5OThlYmMtMTIxMC00MDZmLTg1NzEtNzU4MGJkODc3MTFiNzg1OGRlOTktYjdm';
+
+var str1 = str.split('');
+
+if str1[0]=='/'
 
 request({
     url: 'https://api.ciscospark.com/v1/messages/',
